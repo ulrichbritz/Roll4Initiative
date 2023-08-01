7443a9844569958bf470995a085938122c7c3218
ulrichbritz <ulrich.britz1998@gmail.com>
2023-08-01 16:17:37 -0400

    player head looking at camera lookatpos

---
+++ Di dungeons/Assets/Scripts/Player/PlayerHeadTargetCentered.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UB
+{
+    public class PlayerHeadTargetCentered : MonoBehaviour
+    {
+        private Transform mainCameraTransform;
+        [SerializeField] private float maxDistance = 10f; // Maximum distance to place the target
+
+        private void Start()
+        {
+            // Find the main camera by tag (or you can assign it directly in the Inspector)
+            mainCameraTransform = Camera.main.transform;
+        }
+
+        private void Update()
+        {
+            // Perform a raycast from the camera position along its forward direction
+            if (Physics.Raycast(mainCameraTransform.position, mainCameraTransform.forward, out RaycastHit hit, maxDistance))
+            {
+                // If the raycast hits something, move the target GameObject to the hit point
+                transform.position = hit.point;
+            }
+            else
+            {
+                // If the raycast doesn't hit anything, move the target GameObject to the maximum distance
+                transform.position = mainCameraTransform.position + mainCameraTransform.forward * maxDistance;
+            }
+        }
+    }
+}
+
